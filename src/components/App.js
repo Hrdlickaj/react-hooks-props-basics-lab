cd192ed1b87a1abf3f8b100d5d7c944c4b2a987b
Jenna Hrdlicka <hrdlickajenna@gmail.com>
2022-05-17 16:38:51 -0600

    commit tree aef3778ab7a1f2f58693a0b1cef719d3e13a4145

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
-import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
+import React from 'react';
+import NavBar from './NavBar';
+import Home from './Home';
+import About from './About';
 
 // pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+import user from '../data/user';
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} color={user.color} city={user.city} />
+      <About links={user.links} bio={user.bio} />
     </div>
   );
 }
